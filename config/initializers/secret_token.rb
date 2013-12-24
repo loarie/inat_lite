15eb744f644ab9da148731e905a095b705435a63
Scott Loarie <loarie@gmail.com>
2013-12-24 11:32:02 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InatLite::Application.config.secret_token = '5a85de1b617001f11da52e6f6cedb3bf48c55ece67699872f3790db9c7ad36e8a6753efaf0885a111a57b64fada073e950778e4ed5348455b6f19c10913e5799'
